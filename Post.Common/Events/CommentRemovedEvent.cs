cd5c2272c671aad5b8c8c0a29f831c646a5d0f28
joaopjk <joaopjk48@gmail.com>
2025-06-29 20:56:38 -0300

    Create all events

---
+++ Post.Common/Events/CommentRemovedEvent.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class CommentRemovedEvent : BaseEvent
+    {
+        public Guid CommentId { get; set; }
+        public CommentRemovedEvent() : base(nameof(CommentRemovedEvent)) { }
+    }
+}
