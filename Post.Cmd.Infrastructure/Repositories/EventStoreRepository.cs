b0545d9675f9637ad7fe2b5ed7993d4fb9655436
joaopjk <joaopjk48@gmail.com>
2025-06-29 23:08:17 -0300

    Implements Event Store access to mongo database

---
+++ Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,31 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using MongoDB.Driver;
+
+namespace Post.Cmd.Infrastructure.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> _eventStoreCollection;
+
+        public EventStoreRepository()
+        {
+            var mongoClient = new MongoClient(Environment.GetEnvironmentVariable("MONGO_DB_CONNECTION_STRING"));
+            var mongoDatabase = mongoClient.GetDatabase(Environment.GetEnvironmentVariable("DATABASE"));
+
+            _eventStoreCollection =
+                mongoDatabase.GetCollection<EventModel>(Environment.GetEnvironmentVariable("COLLECTION"));
+        }
+        public async Task SaveAsync(EventModel @event)
+        {
+            await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
